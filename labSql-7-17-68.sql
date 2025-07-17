240119ea60dfa82ddae1d100452e6a884f64f709
Reqiler <123971011+Reqiler@users.noreply.github.com>
2025-07-17 15:38:58 +0700

    Add files via upload

---
+++ labSql-7-17-68.sql
@@ -0,0 +1,11 @@
+Select * From Products;
+
+Select * From Employees;
+Select EmployeeID, FirstName, LastName From Employees;
+Select * From Employees Where City = 'London'; 
+Select EmployeeID, FirstName, LastName From Employees Where City = 'London';
+Select City, Country From Customers
+Select City, Country From Customers
+
+Select * From Categories;
+Select * From Customers;
