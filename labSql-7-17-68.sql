483d5945b1323f19af13dfcc68b2097c5bf49fc3
Reqiler <123971011+Reqiler@users.noreply.github.com>
2025-07-17 15:58:26 +0700

    Update labSql-7-17-68.sql

--- labSql-7-17-68.sql
+++ labSql-7-17-68.sql
@@ -1,11 +1,11 @@
 Select * From Products;
+Select * From Categories;
+Select * From Customers;
 
 Select * From Employees;
 Select EmployeeID, FirstName, LastName From Employees;
 Select * From Employees Where City = 'London'; 
 Select EmployeeID, FirstName, LastName From Employees Where City = 'London';
-Select City, Country From Customers
-Select City, Country From Customers
+Select City, Country From Customers;
+SELECT DISTINCT City, Country From Customers;
 
-Select * From Categories;
-Select * From Customers;
