7639525037534290db6c594d5db76a9cc6560b0f
Reqiler <123971011+Reqiler@users.noreply.github.com>
2025-07-17 16:45:14 +0700

    Update labSql-7-17-68.sql

--- labSql-7-17-68.sql
+++ labSql-7-17-68.sql
@@ -6,6 +6,16 @@ Select * From Employees;
 Select EmployeeID, FirstName, LastName From Employees;
 Select * From Employees Where City = 'London'; 
 Select EmployeeID, FirstName, LastName From Employees Where City = 'London';
-Select City, Country From Customers;
+
+Select City, Country FROM Customers;
 SELECT DISTINCT City, Country From Customers;
 
+SELECT * FROM Products WHERE Unitprice > 200;
+SELECT * FROM Customers WHERE City ='London' OR City ='Vancouver';
+SELECT * FROM Customers WHERE Country = 'USA' OR City = 'Vancouver';
+SELECT * FROM Products WHERE UnitPrice >= 50 AND UnitsInStock < 20;
+SELECT * FROM Products WHERE UnitPrice BETWEEN 100 AND 150;
+SELECT * FROM Products WHERE UnitPrice >= 50 AND UnitPrice<=100;
+SELECT * FROM Customers WHERE Country IN ('Brazil','Argentina','Mexico');
+SELECT * FROM Employees WHERE FirstName LIKE 'N%';
+SELECT * FROM Customers WHERE CompanyName LIKE 'A%';
