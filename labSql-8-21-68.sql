acd3c6597ea6881d23ffee58bafaf2b0d65921e0
reqiler <123971011+reqiler@users.noreply.github.com>
2025-08-21 14:39:03 +0700

    Update labSql-8-21-68.sql

--- labSql-8-21-68.sql
+++ labSql-8-21-68.sql
@@ -34,3 +34,30 @@ SELECT e.EmployeeID, FirstName, o.OrderID FROM Employees as e JOIN Orders as o O
 order by EmployeeID
 
 --
+SELECT O.OrderID เลขใบสั่งซื้อ, C.CompanyName ลูกค้า,
+E.FirstName พนักงาน, O.ShipAddress ส่งไปที่
+FROM Orders O
+join Customers C on O.CustomerID=C.CustomerID
+join Employees E on O.EmployeeID=E.EmployeeID
+
+select e.EmployeeID, FirstName , count(*) as [จำนวน order]
+, sum(freight) as [Sum of Freight]
+from Employees e join Orders o on e.EmployeeID = o.EmployeeID
+where year(orderdate) = 1998
+group by e.EmployeeID, FirstName
+
+--
+SELECT s.CompanyName, count(OrderID) as [จำนวน orders] FROM Shippers as s JOIN Orders as o ON s.ShipperID=o.ShipVia
+GROUP BY s.CompanyName ORDER BY 2 DESC
+
+--
+SELECT p.ProductID, p.ProductName, sum(Quantity) as จำนวนที่ขายได้ FROM Products as p JOIN [Order Details] od on p.ProductID=od.ProductID
+GROUP BY p.ProductID, p.ProductName ORDER BY 3 desc
+
+--
+SELECT distinct p.ProductID, p.ProductName
+FROM Employees as e JOIN Orders as o on e.EmployeeID=o.EmployeeID
+                    JOIN [Order Details] od on o.OrderID=od.OrderID
+                    JOIN Products as p on p.ProductID=od.ProductID
+WHERE e.FirstName = 'Nancy'
+order by ProductID
