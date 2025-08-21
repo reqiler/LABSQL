3e99881e7d055e4cd8f11c162cbf4f8d890426f4
reqiler <123971011+reqiler@users.noreply.github.com>
2025-08-21 13:53:28 +0700

    Update labSql-8-21-68.sql

--- labSql-8-21-68.sql
+++ labSql-8-21-68.sql
@@ -1,2 +1,26 @@
-SELECT CategoryName, ProductName, UnitPrice FROM Products, Categories WHERE Products.CategoryID=Categories.CategoryID;
+--
+SELECT CategoryName, ProductName, UnitPrice FROM Products as p, Categories as c WHERE p.CategoryID=c.CategoryID
+AND CategoryName = 'seafood';
 
+SELECT CategoryName, ProductName, UnitPrice FROM Products as p JOIN Categories as c ON p.CategoryID=c.CategoryID
+WHERE CategoryName = 'seafood';
+
+--
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID = Orders.Shipvia
+
+--
+SELECT CompanyName, OrderID
+FROM Orders, Shippers
+WHERE Shippers.ShipperID = Orders.Shipvia
+AND OrderID = 10275
+
+SELECT CompanyName, OrderID
+FROM Orders JOIN Shippers
+ON Shippers.ShipperID=Orders.Shipvia
+WHERE OrderID=10275
